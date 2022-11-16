e38fca2c1df4f4ee8c4f1a183f8c9a848dd50e05
zTechBoyz <76145948+zTechBoyz@users.noreply.github.com>
2022-11-16 00:37:11 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,54 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+const express = require('express')
+const dayjs = require('dayjs')
+const fs = require('fs')
+const app = express()
+const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello from the server!');
-});
+readBodyAsBuffer = async (req) => {
+    return new Promise((resolve, reject) => {
+        let buffer = Buffer.alloc(0)
+        req.setEncoding(null)
+        req.on(
+            "data",
+            (chunk) => (buffer = Buffer.concat([buffer, Buffer.from(chunk)]))
+        )
+        req.on("end", () => resolve(buffer))
+        req.on("error", reject)
+    })
+}
 
-app.get('/health', (req, res) => {
-  res.sendStatus(200);
-});
 
-app.listen(port, () => {
-  console.log(`app listening at http://localhost:${port}`);
-});
+if (!fs.existsSync('./data')) {
+    fs.mkdirSync('./data')
+}
+
+
+
+app.use(async (req, res, next) => {
+    res.removeHeader('X-Powered-By')
+    res.setHeader('Server', 'openresty')
+    const ip = req.socket.remoteAddress 
+    const tm = dayjs().format('YYYY-MM-DD HH!mm!ss')
+    const info = `[${tm}] [${ip}] ${req.method} ${req.url}`
+
+    const buffer = await readBodyAsBuffer(req)
+    if (buffer && buffer.length) {
+        fs.writeFileSync(`./data/${tm}.txt`, buffer)
+    }
+    fs.writeFileSync(`./data/header.${tm}.txt`, `${info}\n${JSON.stringify(req.headers, null, 2)}`)
+    console.log(info)
+    res.redirect('https://www.google.com/')
+    //next()
+})
+app.all('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.use((req, res) => {
+    res.statusCode = 400
+    res.write('Bad Request')
+    res.end()
+})
+//app.listen(port, '0.0.0.0', () => {
+app.listen(port, () => {    
+    console.log(`Example app listening on port ${port}`)
+})
